ddcf7ef7a110b61c07c603b08c1f00a2c3117792
Timofey Makhlay <tmakhlay2@gmail.com>
2018-09-09 19:43:32 -0700

    Began and did some very MVP layout

---
+++ RealEstate-Calculator/RealEstate-Calculator/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  RealEstate-Calculator
+//
+//  Created by timofey makhlay on 9/9/18.
+//  Copyright © 2018 Timofey Makhlay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
