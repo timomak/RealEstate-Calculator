ba6f2d37ab1d391644760cd9664f6bb8571e00b8
Timofey Makhlay <tmakhlay2@gmail.com>
2018-10-17 11:43:26 -0700

    Added LogIn view [Not Doing anything yet]
    Before adding FireBase API for Login and storage.

---
+++ RealEstate-Calculator/RealEstate-Calculator/Controllers/LogInController.swift
@@ -0,0 +1,16 @@
+//
+//  LogInController.swift
+//  RealEstate-Calculator
+//
+//  Created by timofey makhlay on 10/17/18.
+//  Copyright © 2018 Timofey Makhlay. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
