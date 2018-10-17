33e233ca97d6c23d7c6663de6af73da231d42b4b
Timofey Makhlay <tmakhlay2@gmail.com>
2018-10-17 14:58:52 -0700

    Shitty working version of Sign in

--- RealEstate-Calculator/RealEstate-Calculator/AppDelegate.swift
+++ RealEstate-Calculator/RealEstate-Calculator/AppDelegate.swift
@@ -7,17 +7,55 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Configure Firebase
+        FirebaseApp.configure()
+        // Google account logIn
+        GIDSignIn.sharedInstance().clientID = "1078337616632-a8nu259s1lsj36fif89o57drplkkvokh.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance().delegate = self
+        
 
         return true
+    }
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url as URL?,
+                                                 sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                                                 annotation: options[UIApplication.OpenURLOptionsKey.annotation])
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        print("SignIn function")
+        if let error = error {
+            print("\(error.localizedDescription)")
+        } else {
+            print("Logged in with no errors")
+            // Perform any operations on signed in user here.
+            let userId = user.userID                  // For client-side use only!
+            let idToken = user.authentication.idToken // Safe to send to the server
+            let fullName = user.profile.name
+            let givenName = user.profile.givenName
+            let familyName = user.profile.familyName
+            let email = user.profile.email
+            // ...
+        }
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+              withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- RealEstate-Calculator/RealEstate-Calculator/Controllers/LogInController.swift
+++ RealEstate-Calculator/RealEstate-Calculator/Controllers/LogInController.swift
@@ -7,10 +7,56 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
-class LoginController: UIViewController {
+class LoginController: UIViewController, GIDSignInUIDelegate {
+    @IBOutlet weak var signInButtonByGoogle: GIDSignInButton!
+
+//    @IBAction func signOutButton(_ sender: Any) {
+//        print("Button being pressed")
+//        let firebaseAuth = Auth.auth()
+//        print("This: ",firebaseAuth)
+//        do {
+//            print("Signing Out")
+//            try firebaseAuth.signOut()
+//        } catch let signOutError as NSError {
+//            print ("Error signing out: %@", signOutError)
+//        }
+//    }
+    
+//    @objc func nextVC() {
+//        guard let vc = UIStoryboard(name:"Main", bundle:nil).instantiateViewController(withIdentifier: "register1") as? RegisterViewController1 else {
+//            print("Could not instantiate view controller with identifier of type SecondViewController")
+//            return
+//        }
+//        self.navigationController?.pushViewController(vc, animated:true)
+//    }
+    var handle: AuthStateDidChangeListenerHandle?
+    //initializes the log in button
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signInSilently()
+
+        // TODO(developer) Configure the sign-in button look/feel
+        // ...
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // [START auth_listener]
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            
+        }
+        // [END auth_listener]
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // [START remove_auth_listener]
+        Auth.auth().removeStateDidChangeListener(handle!)
+        // [END remove_auth_listener]
+    }
 }
