04d58ffa113c287ec6cdb4ba7b2370cd67f01858
Timofey Makhlay <tmakhlay2@gmail.com>
2018-10-18 15:17:04 -0700

    Everything working. Trying to send data to database

--- RealEstate-Calculator/RealEstate-Calculator/AppDelegate.swift
+++ RealEstate-Calculator/RealEstate-Calculator/AppDelegate.swift
@@ -34,6 +34,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                                      sourceApplication:options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
                                                      annotation: [:])
     }
+    
+    //iOS 8 and older
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url,
+                                                 sourceApplication: sourceApplication,
+                                                 annotation: annotation)
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -55,9 +62,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-//        print("app closing")
-//        let login = LoginController()
-//        var savingDataToFirebaseDatabase = login.appClosingSaveDataToFireBase()
+        print("app closing")
+        let login = LoginController()
+        var savingDataToFirebaseDatabase = login.appClosingSaveDataToFireBase()
     }
 
 
--- RealEstate-Calculator/RealEstate-Calculator/Controllers/LogInController.swift
+++ RealEstate-Calculator/RealEstate-Calculator/Controllers/LogInController.swift
@@ -12,10 +12,9 @@ import GoogleSignIn
 
 // TODO: Handle UserDefualts sync from Firebase to Userdefaults after Login
 
-class LoginController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
+class LoginController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate  {
     // Connect to Firebase Database
-//    var ref: DatabaseReference!
-    @IBOutlet weak var signInButtonByGoogle: GIDSignInButton!
+    var ref: DatabaseReference!
     var uid: String?
     var username: String?
     var email: String?
@@ -23,51 +22,105 @@ class LoginController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupGoogleButtons()
         // Connect Google Sign in from delegate and Sign in user.
-        GIDSignIn.sharedInstance().uiDelegate = self
-        GIDSignIn.sharedInstance().delegate = self
-        GIDSignIn.sharedInstance().signInSilently()
         
         // TODO: Configure the sign-in button look/feel
     }
+    fileprivate func setupGoogleButtons(){
+        // add google sign in button
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        
+        // automatically sign in the user.
+        GIDSignIn.sharedInstance().signInSilently()
+        
+        // TODO(developer) Configure the sign-in button look/feel
+        // ...
+    }
+    //
+    //    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+    //        // ...
+    //        if let error = error {
+    //            print("ERROR in SIGNIN")
+    //            return
+    //        }
+    //        print("Sign IN WORKED")
+    //        guard let authentication = user.authentication else { return }
+    //        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+    //                                                       accessToken: authentication.accessToken)
+    //        if Auth.auth().currentUser != nil {
+    //            // User is signed in.
+    //            let user = Auth.auth().currentUser
+    //
+    //            if let user = user {
+    //                print("User found and Authenticated.")
+    //                // Save user info in session
+    //                uid = user.uid
+    //                username = user.displayName
+    //                email = user.email
+    ////                let photoURL = user.photoURL
+    //                print("email: ", email!)
+    //
+    //                // Connect to Database once logged in before segue
+    ////                ref = Database.database().reference()
+    //                // self.ref.child("users").child(user.uid).setValue(["username": username])
+    //
+    //                // Read saved Userdefaults and save/write into Firebase Database.
+    ////                savePropertyArrayToFirebaseDatabase(userId: uid!, username: username!, dictionary: UserDefaults.standard.array(forKey: "properties") as! [[String : [String : Double]]])
+    //
+    //                // Segue to the next view if User is signed in.
+    //                self.performSegue(withIdentifier: "toAllProperties", sender: self)
+    //            }
+    //        } else {
+    //            // No user is signed in.
+    //            print("No USER FOUND")
+    //        }
+    //    }
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
         // ...
         if let error = error {
-            print("ERROR in SIGNIN")
+            print("Failed to login to Google")
+            
             return
         }
-        print("Sign IN WORKED")
+        
+        print("successfully logged into Google", user)
+        
         guard let authentication = user.authentication else { return }
         let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
                                                        accessToken: authentication.accessToken)
-        if Auth.auth().currentUser != nil {
-            // User is signed in.
-            let user = Auth.auth().currentUser
-            
-            if let user = user {
-                print("User found and Authenticated.")
-                // Save user info in session
-                uid = user.uid
-                username = user.displayName
-                email = user.email
-//                let photoURL = user.photoURL
-                print("email: ", email!)
-                
-                // Connect to Database once logged in before segue
-//                ref = Database.database().reference()
-                // self.ref.child("users").child(user.uid).setValue(["username": username])
-                
-                // Read saved Userdefaults and save/write into Firebase Database.
-//                savePropertyArrayToFirebaseDatabase(userId: uid!, username: username!, dictionary: UserDefaults.standard.array(forKey: "properties") as! [[String : [String : Double]]])
-                
-                // Segue to the next view if User is signed in.
-                self.performSegue(withIdentifier: "toAllProperties", sender: self)
+        
+        
+        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+            if let error = error {
+                print("Problem at signing in with google with error : \(error)")
+                return
             }
-        } else {
-            // No user is signed in.
-            print("No USER FOUND")
+            // User is signed in
+            self.username = Auth.auth().currentUser?.displayName
+            self.email = Auth.auth().currentUser?.email
+            self.uid = Auth.auth().currentUser?.uid
+            print("user successfully signed in through GOOGLE! uid:\(Auth.auth().currentUser!.email)")
+            
+            // Connect firebase database
+            self.ref = Database.database().reference()
+            
+            // Testing if database works.
+            //            self.ref.child("users").child(self.uid!).setValue(["username": self.username!])
+            
+            // Read saved Userdefaults and save/write into Firebase Database.
+//            if UserDefaults.standard.array(forKey: "properties") != nil {
+//                self.savePropertyArrayToFirebaseDatabase(userId: self.uid!, username: self.username!, dictionary: UserDefaults.standard.array(forKey: "properties") as! [[String : [String : Double]]])
+//            }
+            
+            
+            
+            print("signed in")
+            self.performSegue(withIdentifier: "toAllProperties", sender: self)
         }
+        
     }
     
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
@@ -76,53 +129,60 @@ class LoginController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate 
         print("This is now working. SIGNOUT")
     }
     
-//    // Read saved Userdefaults and save/write into Firebase Database.
-//    func savePropertyArrayToFirebaseDatabase(reference: DatabaseReference, userId: String, username: String, dictionary: [[String: [String: Double]]]) {
-//        for array in dictionary {
-//            for (name, dict) in array {
-//                let newProperty = Property()
-//                newProperty.name = name
-//                reference.child("users/\(userId)/\(username)/property").setValue(name)
-//                for (nameOfValue, value) in dict {
-//                    if nameOfValue == "price" {
-//                        newProperty.buyingPrice = value
-//                        reference.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
-//                        reference.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
-//                    } else if nameOfValue == "rent" {
-//                        newProperty.rent = value
-//                        reference.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
-//                        reference.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
-//                    }
-//                    else if nameOfValue == "buildingTax" {
-//                        newProperty.buildingTax = value
-//                        reference.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
-//                        reference.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
-//                    }
-//                    else if nameOfValue == "propertyTax" {
-//                        newProperty.propertyTax = value
-//                        reference.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
-//                        reference.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
-//                    }
-//                    else if nameOfValue == "fees" {
-//                        newProperty.yearlyFees = value
-//                        reference.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
-//                        reference.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
-//                    }
-//                    else if nameOfValue == "growth" {
-//                        newProperty.valueGrowth = value
-//                        reference.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
-//                        reference.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
-//                    }
-////                    print("Name: \(name) [\(nameOfValue): \(value)]")
-//                }
-//                //                properties.append(newProperty)
-//            }
+    // Read saved Userdefaults and save/write into Firebase Database.
+    func savePropertyArrayToFirebaseDatabase(userId: String, username: String, dictionary: [[String: [String: Double]]]) {
+        
+        if dictionary == nil {
+            return
+        }
+        for array in dictionary {
+            for (name, dict) in array {
+                let newProperty = Property()
+                newProperty.name = name
+                self.ref.child("users/\(userId)/\(username)/property").setValue(name)
+                for (nameOfValue, value) in dict {
+                    if nameOfValue == "price" {
+                        newProperty.buyingPrice = value
+                        self.ref.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
+                        self.ref.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
+                    } else if nameOfValue == "rent" {
+                        newProperty.rent = value
+                        self.ref.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
+                        self.ref.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
+                    }
+                    else if nameOfValue == "buildingTax" {
+                        newProperty.buildingTax = value
+                        self.ref.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
+                        self.ref.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
+                    }
+                    else if nameOfValue == "propertyTax" {
+                        newProperty.propertyTax = value
+                        self.ref.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
+                        self.ref.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
+                    }
+                    else if nameOfValue == "fees" {
+                        newProperty.yearlyFees = value
+                        self.ref.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
+                        self.ref.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
+                    }
+                    else if nameOfValue == "growth" {
+                        newProperty.valueGrowth = value
+                        self.ref.child("users/\(userId)/\(username)/\(name)/nameOfvalue").setValue(nameOfValue)
+                        self.ref.child("users/\(userId)/\(username)/\(name)/\(nameOfValue)/value").setValue(value)
+                    }
+                    //                    print("Name: \(name) [\(nameOfValue): \(value)]")
+                }
+                //                properties.append(newProperty)
+            }
+        }
+    }
+    
+    func appClosingSaveDataToFireBase() {
+        print("I AM SAVING THE DATA TO DATABASE")
+        // Read saved Userdefaults and save/write into Firebase Database.
+//        if UserDefaults.standard.array(forKey: "properties") != nil {
+//            LoginController.savePropertyArrayToFirebaseDatabase(userId: LoginController.uid!, username: LoginController.username!, dictionary: UserDefaults.standard.array(forKey: "properties") as! [[String : [String : Double]]])
 //        }
-//    }
-//
-//    func appClosingSaveDataToFireBase() {
-//        print("I AM SAVING THE DATA TO DATABASE")
-//        savePropertyArrayToFirebaseDatabase(reference: ref!, userId: uid!, username: username!, dictionary: UserDefaults.standard.array(forKey: "properties") as! [[String : [String : Double]]])
-//    }
+    }
 }
 
